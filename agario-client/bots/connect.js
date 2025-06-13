4eb3151b758c987a51f7311c79aacf7accbb0e2a
bomioz <navarrochristopher068@gmail.com>
2025-06-13 01:02:11 -0400

    Update connect.js

--- agario-client/bots/connect.js
+++ agario-client/bots/connect.js
@@ -1,11 +1,36 @@
-const createBot = require('./agario-client');
+const EventEmitter = require('events');
 
-function connectBots(region, partyCode, amount = 1) {
-  for (let i = 0; i < amount; i++) {
-    setTimeout(() => {
-      createBot(region, partyCode);
-    }, i * 1000);
-  }
+class AgarClient extends EventEmitter {
+    constructor() {
+        super();
+        this.connected = false;
+        this.nick = '';
+    }
+
+    connect(region, partyCode) {
+        this.connected = true;
+        setTimeout(() => {
+            this.emit('connected');
+        }, 1000);
+    }
+
+    spawn(nick) {
+        this.nick = nick;
+        this.emit('spawned', nick);
+    }
+
+    split() {
+        this.emit('split');
+    }
+
+    eject() {
+        this.emit('eject');
+    }
+
+    disconnect() {
+        this.connected = false;
+        this.emit('disconnected');
+    }
 }
 
-module.exports = connectBots;
+module.exports = AgarClient;
