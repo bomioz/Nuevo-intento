fb6063c003c6e2be345bad23e8bd77351926d122
bomioz <navarrochristopher068@gmail.com>
2025-06-13 00:30:17 -0400

    Create connect.js

---
+++ bots/connect.js
@@ -0,0 +1,10 @@
+const { Client } = require('discord.js');
+
+function connectBots(partyCode, region) {
+  console.log(`Conectando bots a la party ${partyCode} en la región ${region}`);
+  
+  // Aquí es donde eventualmente se conectarán los bots reales
+  // Por ahora puedes simularlo con un mensaje de consola
+}
+
+module.exports = { connectBots };
