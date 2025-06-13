1de0c63e5860ece8e5a36fa04ff0fdfa8dff0fee
bomioz <navarrochristopher068@gmail.com>
2025-06-13 00:12:29 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -14,3 +14,19 @@ client.on('messageCreate', message => {
     message.channel.send(`Conectando ${cantidad} bot(s) a la party ${party} en ${region}`);
   }
 });
+const { createBot } = require('./bots/connect');
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('!bots')) {
+    const args = message.content.split(' ');
+    const region = args[1]; // Ej: west
+    const party = args[2];  // Ej: NPEJCN
+    const cantidad = parseInt(args[3]) || 1;
+
+    for (let i = 0; i < cantidad; i++) {
+      createBot({ region, party, name: `Bot${i + 1}` });
+    }
+
+    message.channel.send(`🚀 Conectando ${cantidad} bot(s) a la party ${party} en región ${region}`);
+  }
+});
