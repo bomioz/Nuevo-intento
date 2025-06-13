0af3bfd28b75eeb8540774d25cf60523608db255
bomioz <navarrochristopher068@gmail.com>
2025-06-12 23:09:10 -0400

    Update index.js

--- agario-client/index.js
+++ agario-client/index.js
@@ -1,1 +1,25 @@
+// Simulación básica de agario-client
+class AgarioClient {
+  constructor() {
+    console.log('AgarioClient cargado');
+  }
 
+  connect(serverURL, partyCode) {
+    console.log(`🔗 Conectando a Agar.io en: ${serverURL} con código de party: ${partyCode}`);
+    // Aquí va el código de conexión real
+  }
+
+  followPlayer(id) {
+    console.log(`➡️ Siguiendo al jugador con ID: ${id}`);
+  }
+
+  split() {
+    console.log("💥 Split ejecutado");
+  }
+
+  feed() {
+    console.log("🍕 Feeding...");
+  }
+}
+
+module.exports = AgarioClient;
